425959b4fb471bec48a37768d625946a34d9ded7
chika <fumifumibot2323@gmail.com>
2023-11-12 16:19:35 +0900

    yamlファイル構造修正に対応

--- narocheckerbot/config_manager.py
+++ narocheckerbot/config_manager.py
@@ -10,21 +10,21 @@ class ConfigManager:
 
     def __init__(self) -> None:
         """初期化."""
-        self.logger = getLogger("narocheckerlog.config")
-        self.configfile = os.path.dirname(os.path.abspath(__file__)) + "/config.yaml"
+        self._logger = getLogger("narocheckerlog.config")
+        self._configfile = os.path.dirname(os.path.abspath(__file__)) + "/config.yaml"
 
-        with open(self.configfile, "r") as stream:
+        with open(self._configfile, "r") as stream:
             yaml = YAML()
-            self.yaml_data = yaml.load(stream)
+            self._yaml_data = yaml.load(stream)
 
         # self.channel_id = self.yaml_data["channel"]
         pass
 
     def write_yaml(self):
         """設定ファイルへの書き込み."""
-        with open(self.configfile, "w") as stream:
+        with open(self._configfile, "w") as stream:
             yaml = YAML()
-            yaml.dump(data=self.yaml_data, stream=stream)
+            yaml.dump(data=self._yaml_data, stream=stream)
 
     def get_urls(self) -> List[Dict[Any, Any]]:
         """URLリストを返す.
@@ -32,7 +32,7 @@ class ConfigManager:
         Returns:
             Dict[Any, Any]: _description_
         """
-        return self.yaml_data["account"]
+        return self._yaml_data["naro"]["account"]
 
     def get_channel_id(self) -> int:
         """結果を書き込むチャンネルIDを返す.
@@ -40,6 +40,6 @@ class ConfigManager:
         Returns:
             int: チャンネルID
         """
-        return self.yaml_data["channel"]
+        return self._yaml_data["naro"]["channel"]
 
     pass
